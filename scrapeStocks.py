31254eaf6d5f496172b90acc8fd04801c8d6236a
Navdeep <10390511+sivunq@users.noreply.github.com>
2020-12-09 17:00:27 +0530

    updating cell styling

--- scrapeStocks.py
+++ scrapeStocks.py
@@ -1,5 +1,5 @@
 import urllib,requests,openpyxl
-from openpyxl.styles import colors,Color, PatternFill, Font, Border
+from openpyxl.styles import colors,Color, PatternFill, Font, Border,Side,Alignment
 from openpyxl.cell import Cell
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -34,40 +34,54 @@ while(True):
         break
     todayRow=todayRow+1
 
-index=0
 total=0
 headers = {"user-agent" : "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"}
-for stock in stocks:
+for index in range(1,len(stocks)+4):
+    cellFill=sheet.cell(row=todayRow,column=index)
+    thin_border = Border(left=Side(style='thin'), right=Side(style='thin'),top=Side(style='thin'),bottom=Side(style='thin'))
+    cellFill.border = thin_border
+    cellFill.alignment = Alignment(horizontal='center')
+    lightPink = openpyxl.styles.colors.Color(rgb='FCE4D6')
+    lightGreen = openpyxl.styles.colors.Color(rgb='A9D08E')
+    darkGreen = openpyxl.styles.colors.Color(rgb='70AD47')
+    
     #money control has two ways of showing stock prices, both handled seperately
-    if(way[index]=="new"):
-        resp = requests.get(link[index], headers=headers)
-        soup = BeautifulSoup(resp.content, "html.parser")
-        mydivs = soup.findAll("div", {"class": "nsecp"})
-        price=mydivs[0]["rel"]
-    elif(way[index]=="old"):
-        resp = requests.get(link[index], headers=headers)
-        soup = BeautifulSoup(resp.content, "html.parser")
-        mydivs = soup.findAll("span", {"class": "span_price_wrap"})
-        price=mydivs[1].text
-    price=(float(price)-initialPrice[index])*sharesCount[index]
-    price= round(price,2)
-    total=total+price
-    sheet.cell(row=todayRow,column=index+2).value=price
-    print(index+1,".",stock,":",price)
-    cellFill=sheet.cell(row=todayRow,column=index+2)
+    if(index>1 and index <= len(stocks)+1):
+        if(way[index-2]=="new"):
+            resp = requests.get(link[index-2], headers=headers)
+            soup = BeautifulSoup(resp.content, "html.parser")
+            mydivs = soup.findAll("div", {"class": "nsecp"})
+            price=mydivs[0]["rel"]
+        elif(way[index-2]=="old"):
+            resp = requests.get(link[index-2], headers=headers)
+            soup = BeautifulSoup(resp.content, "html.parser")
+            mydivs = soup.findAll("span", {"class": "span_price_wrap"})
+            price=mydivs[1].text
+        price=(float(price)-initialPrice[index-2])*sharesCount[index-2]
+        price= round(price,2)
+        total=total+price
+        sheet.cell(row=todayRow,column=index).value=price
+        print(index-1,".",stocks[index-2],":",price)
 
-    #make cell pink color if today's price is lesser than yesterday
-    if(price < (sheet.cell(row=todayRow-1,column=index+2).value)):
-        myColor = openpyxl.styles.colors.Color(rgb='FCE4D6')
-        myFill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=myColor)
-        cellFill.fill = myFill
+        if(price < (sheet.cell(row=todayRow-1,column=index).value)):
+            myColor=lightPink
+        else:
+            myColor=lightGreen
+    else:
+        myColor=darkGreen
+		
+    myFill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=myColor)
+    cellFill.fill = myFill
             
     index=index+1
+
 #update total and day change
 total=round(total,2)
-sheet.cell(row=todayRow,column=index+2).value=total
-prevTotal=sheet.cell(row=todayRow-1,column=index+2).value
-sheet.cell(row=todayRow,column=index+3).value=total-prevTotal
+sheet.cell(row=todayRow,column=index-2).value=total
+
+prevTotal=sheet.cell(row=todayRow-1,column=index-2).value
+sheet.cell(row=todayRow,column=index-1).value=total-prevTotal
+
 wb.save("FinTrack.xlsx")
 wb.close()
 print("Done")
