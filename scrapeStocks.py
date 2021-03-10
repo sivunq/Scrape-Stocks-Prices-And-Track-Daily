82956f124d0262fbe544c5a669cfc9fc3840fe74
Navdeep <10390511+sivunq@users.noreply.github.com>
2021-03-10 20:23:06 +0530

    updated difference row values

--- scrapeStocks.py
+++ scrapeStocks.py
@@ -68,7 +68,7 @@ for index in range(1,len(stocksData["stockNames"])+4):
         colIndex=openpyxl.utils.cell.get_column_letter(index)
         sheet.conditional_formatting.add(colIndex+str(8)+":"+colIndex+str(todayRow) ,ColorScaleRule(start_type='min', start_value=0, start_color='F75E68',
                                             mid_type='percentile', mid_value=50, mid_color='FFFA99',
-                                            end_type='max', end_value=100, end_color='60B37F'))
+                                            end_type='max', end_value=100, end_color='36A460'))
 	
     #style each column
     cellFill=sheet.cell(row=todayRow,column=index)
